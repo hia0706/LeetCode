b7edb90f3f3f82c9524f1165458aa491bed41391
hia0706 <ljjs1215@gmail.com>
2024-01-08 13:43:28 +0900

    Feat 문제 풀이

--- nklcb-ex/eng_quiz.java
+++ nklcb-ex/eng_quiz.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class eng_quiz {
 
     public static void main(String[] args) {
@@ -11,6 +14,27 @@ public class eng_quiz {
     }
 
     static int solution(String s) {
+        // 단어를 숫자로 매핑하는 해시맵 생성
+        HashMap<String, String> wordToNumber = new HashMap<>();
+        wordToNumber.put("zero", "0");
+        wordToNumber.put("one", "1");
+        wordToNumber.put("two", "2");
+        wordToNumber.put("three", "3");
+        wordToNumber.put("four", "4");
+        wordToNumber.put("five", "5");
+        wordToNumber.put("six", "6");
+        wordToNumber.put("seven", "7");
+        wordToNumber.put("eight", "8");
+        wordToNumber.put("nine", "9");
+
+        // 반복문으로 wordToNumber의 각 항목을 반복해서 해당 단어를 숫자로 치환한다.
+        for (Map.Entry<String, String> entry : wordToNumber.entrySet()) {
+            // 문자열 's' 의 모든 발생을 해당하는 숫자로 바꾼다.
+            s = s.replace(entry.getKey(), entry.getValue());
+        }
+
+        // 변환된 문자열을 정수로 변환해서 answer 변수에 담아서 값을 반환한다.
+        int answer = Integer.parseInt(s);
 
         return answer;
     }
