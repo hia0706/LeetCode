d7f264158bd7580206a6cc438988a347c017d668
hia0706 <ljjs1215@gmail.com>
2024-01-08 12:48:11 +0900

    Init 네카라쿠배 문자열, 구현 문제 수정본

---
+++ nklcb-ex/eng_quiz.java
@@ -0,0 +1,17 @@
+public class eng_quiz {
+
+    public static void main(String[] args) {
+        String s1 = "8zerothree2";
+        String s2 = "seven73nine";
+        String s3 = "two53eightfour";
+
+        System.out.println(solution(s1)); // 8032
+        System.out.println(solution(s2)); // 7739
+        System.out.println(solution(s3)); // 25384
+    }
+
+    static int solution(String s) {
+
+        return answer;
+    }
+}
